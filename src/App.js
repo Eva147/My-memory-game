17a5b5558166cfd281e9a239077d3d9216006ec5
Evgeniya <eva147@list.ru>
2022-06-23 14:27:26 +0100

    Add random dublicated cards, wallpeper image, add a new game to button and counter of turns for player

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 const cardImages = [
@@ -10,10 +11,42 @@ const cardImages = [
 ]
 
 function App() {
+ // cards for game
+  const [cards, setCards] = useState([])
+// player's turns
+  const [turns, setTurns] = useState(0);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    // we need to take each card twice, so we have 12 cards in total
+    const shuffledCards = [...cardImages, ...cardImages]
+    // sort images to mix them up
+    .sort(() => Math.random() - 0.5)
+    .map(card => ({ ...card, id: Math.random() }))
+
+    setCards(shuffledCards)
+    setTurns(0)
+  } 
+
+  console.log(cards, turns);
+
+
   return (
     <div className="App">
       <h1>My Memory Game</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className='card-grid'>
+        {cards.map(card => (
+          <div className='card' key={card.id}>
+            <div>
+              <img className='front' src={card.src} alt='card front' />
+              <img className='back' src='img/wallpapers.png' alt='card back' />
+            </div>
+          </div>
+        )) }
+      </div>
+
     </div>
   );
 }
